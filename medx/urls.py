74ddb979182834884fbed0776ad864c76018f842
opeodedeyi <opeyemiodedeyi@gmail.com>
2020-02-28 14:31:55 +0100

    Update urls.py

--- medx/urls.py
+++ medx/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('accounts.urls')),
+    path('', include('accounts.urls')),
 ]
